502322447b0b20637a828695a3461c1d8fb1d78a
natalia-kk <nataliakloppers@yahoo.com.au>
2021-07-27 18:39:00 +1000

    get by single id nutrition route created and working

--- routes/nutrition.js
+++ routes/nutrition.js
@@ -12,7 +12,7 @@ const path = require('path')
 router.get('/', (req, res) => {
     
         
-  // get all products from the Nutrition model using the find() method
+  // get all nutrition information docs from the Nutrition model using the find() method
   Nutrition.find()
       // if successful will return json object containing list of all messages (messages)
       .then((nutritions) => {
@@ -23,8 +23,32 @@ router.get('/', (req, res) => {
       .catch((err) => {
           console.log("problem getting nutrition information", err)
       })
-
 })
+
+// GET - get single nutrition doc
+router.get('/:id', (req, res) => {
+    // use the Nutrition model to find one doc by id
+    Nutrition.findById(req.params.id)
+        .then((nutrition) => {
+            // check if nutrition doc with that id exists
+            // if it's false/not found, send status code + json mesage
+            if(!nutrition) {
+                res.status(400).json({
+                    message: "nutrition information document doesn't exist"
+                })
+            // now normal json response will only run if nutrition object is found successfully
+            }else{
+                res.json(nutrition)
+            }
+        })
+        .catch((err) => {
+            console.log("error getting nutritional information", err)
+                res.status(500).json({ // error response sent to user
+                    message: "problem getting nutritional information",
+                    error: err
+            })
+        })
+    })
 
 // EXPORT the router object 
 // (imported in server.js in 'ROUTES' section)
